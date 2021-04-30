2b8ef9cf8de53a54695ce4e4c87f9b3afcf4b3a8
noxonxno <2854770587@qq.com>
2021-04-30 17:21:07 +0800

    yjx

---
+++ src/views/plan/mesAdvice/mesAdviceTable.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-form :inline="true"
+               :model="parameterDto"
+               class="demo-form-inline"
+               @keyup.enter.native="loadPageTableList">
+        <el-input v-model="parameterDto.deviceName"
+                  placeholder="MES通知总计划数"
+                  style="width: 150px;"
+                  class="filter-item"
+                  maxlength="32" />
+        <el-button v-waves
+                   class="filter-item"
+                   type="primary"
+                   icon="el-icon-search"
+                   @click="loadPageTableList">搜索</el-button>
+        <el-button class="filter-item"
+                   style="margin-left: 10px;"
+                   type="primary"
+                   icon="el-icon-edit"
+                   @click="handleCreate(false)">导入</el-button>
+      </el-form>
+
+    </div>
+
+    <el-table :data="mesAdviceList"
+              row-key="id"
+              border>
+      <el-table-column label="创建时间">
+        <template slot-scope="scope">
+          <span>{{ scope.row.deviceCode }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建用户">
+        <template slot-scope="scope">
+          <span>{{ scope.row.deviceName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="计划状态">
+        <template slot-scope="scope">
+          <p v-if="scope.row.deviceState ==0">正常</p>
+          <p v-if="scope.row.deviceState ==1">运行中</p>
+          <p v-if="scope.row.deviceState ==2">空闲</p>
+          <p v-if="scope.row.deviceState ==3">异常</p>
+        </template>
+      </el-table-column>
+      <el-table-column label="序号">
+        <template slot-scope="scope">
+          <span>{{ scope.row.updateTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :label="$t('table.actions')"
+                       width="300">
+        <template slot-scope="scope">
+          <el-button type="primary"
+                     size="mini"
+                     @click="handleCreate(scope.row)">错误上报
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total>0"
+                :total="total"
+                :page.sync="parameterDto.current"
+                :limit.sync="parameterDto.size"
+                @pagination="loadPageTableList" />
+
+    <el-dialog :title="textMap[dialogStatus]"
+               :visible.sync="dialogFormVisible">
+      <el-form ref="submitForm"
+               :model="submitForm"
+               label-width="100px"
+               class="organizationForm"
+               style="width: 400px; margin-left:50px;">
+        <el-form-item label="设备编码"
+                      prop="deviceCode">
+          <el-input v-model.trim="submitForm.deviceCode"
+                    placeholder="输入设备编码"
+                    maxlength="32" />
+        </el-form-item>
+        <el-form-item label="设备名称"
+                      prop="deviceName">
+          <el-input v-model.trim="submitForm.deviceName"
+                    placeholder="输入设备名称"
+                    maxlength="32" />
+        </el-form-item>
+        <el-form-item label="设备类型"
+                      prop="deviceType">
+          <el-input v-model.trim="submitForm.deviceType"
+                    oninput="value=value.replace(/^\.+|[^\d.]/g,'')"
+                    placeholder="输入设备类型(输入编号)" />
+          <div class="sub-title"
+               style="color:red">0:辊道 1:天车 2:喷码机 3:切割机 4:举升机 5:小件分拣机器人 6:大件分拣机器人 7:沙光机 8:皮带输送线 9:入筐机器人</div>
+        </el-form-item>
+      </el-form>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">{{ $t('table.cancel') }}</el-button>
+        <el-button v-if="dialogStatus=='create'"
+                   type="primary"
+                   @click="createData">{{ $t('table.confirm') }}</el-button>
+        <el-button v-else
+                   type="primary"
+                   @click="updateData">{{ $t('table.confirm') }}</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+/**
+  Auth: Lei.j1ang
+  Created: 2018/1/19-14:54
+*/
+import { selectDeviceList, addDevice, updateDevice, deleteDevice } from '@/api/plan/mesAdivce'
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+import waves from '@/directive/waves' // 水波纹指令
+
+export default {
+  name: 'device',
+  components: { Pagination },
+  data: () => ({
+    mesAdviceList: [],
+    total: 0,
+    parameterDto: {
+      deviceName: "",
+      current: 1,
+      size: 10,
+    },
+    textMap: {
+      update: '编辑',
+      create: '添加'
+    },
+    dialogStatus: '',
+    dialogFormVisible: false,
+    submitForm: {
+      deviceCode: "",
+      deviceName: "",
+      deviceState: ""
+    }
+  }),
+  directives: {
+    waves
+  },
+  created () {
+    this.loadPageTableList();
+  },
+  methods: {
+    loadPageTableList () {
+      this.listLoading = true
+      selectDeviceList(this.parameterDto).then(response => {
+        this.mesAdviceList = response.data
+        this.total = response.count
+        this.listLoading = false
+      })
+    },
+    handleCreate () {
+      this.deviceCode = ""
+      this.deviceName = ""
+      this.deviceState = ""
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.submitForm = {}
+
+    },
+    createData () {
+      addDevice(this.submitForm).then(response => {
+        if (response.code == 200) {
+          this.$message({
+            message: '新增成功',
+            type: 'success'
+          })
+          this.loadPageTableList();
+          this.$refs['submitForm'].resetFields()
+          this.dialogFormVisible = false
+        } else {
+          this.$message({
+            message: '新增失败',
+            type: 'error'
+          })
+        }
+      })
+    },
+    handleUpdate (row) {
+      this.submitForm = Object.assign({}, row) //Object.assign方法用于对象的合并 复制到目标对象
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+    },
+    updateData () {
+      updateDevice(this.submitForm).then(response => {
+        if (response.code == 200) {
+          this.$message({
+            message: '修改成功',
+            type: 'success'
+          })
+          this.loadPageTableList();
+          this.$refs['submitForm'].resetFields()
+          this.dialogFormVisible = false
+        } else {
+          this.$message({
+            message: '修改失败',
+            type: 'error'
+          })
+        }
+      })
+    },
+    handleDelete (id) {
+      this.$confirm(
+        '删除该设备, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).then(() => {
+        this.listLoading = true
+        deleteDevice(id).then(response => {
+          if (response.code == 200) {
+            this.listLoading = false
+            this.loadPageTableList();
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            })
+          } else {
+            this.$message({
+              message: '删除失败',
+              type: 'error'
+            })
+          }
+        })
+      })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    }
+  }
+}
+</script>
+
+
+
